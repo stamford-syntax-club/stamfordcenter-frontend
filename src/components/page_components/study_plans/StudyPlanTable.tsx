082c6db500e1433726e8fb33b45838755cb1d1e6
Lxkas <alohasunshineday@yahoo.com>
2023-08-10 22:42:32 +0700

    implement study_plans page

---
+++ src/components/page_components/study_plans/StudyPlanTable.tsx
@@ -0,0 +1,141 @@
+import { ActionIcon, Checkbox, Table, TextInput, Tooltip } from "@mantine/core";
+import { useDebouncedState, useListState } from "@mantine/hooks";
+import { StudyPlanElement } from "@ourtypes/study_plans_page_types/StudyPlanElement";
+import { useState } from "react";
+import { FaDownload, FaEye, FaSearch } from "react-icons/fa";
+import { StudyPlanTableRowsTemplate } from "./StudyPlanTableRowsTemplate";
+
+interface StudyPlanTableProps {
+	elements: StudyPlanElement[];
+}
+
+// XD
+async function downloadFile(element: StudyPlanElement) {
+	const fileUrl = `https://center-be.stamford.dev/api/get_files/${element.fileKey}?download=true`;
+
+	try {
+		const response = await fetch(fileUrl);
+
+		if (!response.ok) {
+			console.error("Network response was not OK", response);
+			return;
+		}
+
+		const blob = await response.blob();
+		const url = window.URL.createObjectURL(blob);
+
+		const a = document.createElement("a");
+		a.href = url;
+		a.download = `${element.major}_${element.year}_${element.language}.pdf`;
+		a.style.display = "none"; // Hide the element from view
+
+		document.body.appendChild(a);
+		a.click();
+		document.body.removeChild(a); // Remove the element from the DOM
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+async function previewFile(element: StudyPlanElement) {
+	window.open(`https://center-be.stamford.dev/api/get_files/${element.fileKey}`, "_blank");
+}
+
+export function StudyPlanTable({ elements }: StudyPlanTableProps) {
+	const [values, handlers] = useListState(elements);
+	const [searchString, setSearchString] = useDebouncedState("", 500);
+
+	const [downloading, setDownloading] = useState(false);
+
+	const allChecked = values.every((value) => value.checked);
+	const indeterminate = values.some((value) => value.checked) && !allChecked;
+
+	function getAllCheckedStudyPlanElements() {
+		return values.filter((value) => value.checked);
+	}
+
+	async function handleDownload() {
+		if (downloading) return;
+		setDownloading(true);
+
+		const checkedStudyPlanElements = getAllCheckedStudyPlanElements();
+
+		for (const element of checkedStudyPlanElements) {
+			await downloadFile(element);
+		}
+
+		setDownloading(false);
+	}
+
+	function handlePreview() {
+		const checkedStudyPlanElements = getAllCheckedStudyPlanElements();
+		// TODO: preview all in new tabs
+		console.log("Preview", checkedStudyPlanElements);
+
+		for (const element of checkedStudyPlanElements) {
+			previewFile(element);
+		}
+	}
+
+	const filteredValues = values.filter((value) => value.major.toLowerCase().includes(searchString.toLowerCase()));
+
+	function handleAllCheckedChange(event: React.ChangeEvent<HTMLInputElement>) {
+		handlers.setState((current) => current.map((value) => ({ ...value, checked: !allChecked })));
+	}
+
+	return (
+		<Table striped highlightOnHover>
+			<caption>
+				<div className="flex flex-row items-center">
+					{/* Table search input */}
+					<div className="ml-2">
+						<TextInput
+							icon={<FaSearch />}
+							placeholder="Search in table"
+							variant="filled"
+							onChange={(event) => setSearchString(event.currentTarget.value)}
+						/>
+					</div>
+
+					{/* Table action buttons, download/preview */}
+					<div className="ml-auto mr-2 flex flex-row gap-x-2">
+						<Tooltip.Group openDelay={500}>
+							<Tooltip label="Download">
+								<ActionIcon disabled={downloading} variant="filled" onClick={handleDownload}>
+									<FaDownload />
+								</ActionIcon>
+							</Tooltip>
+
+							<Tooltip label="Preview">
+								<ActionIcon variant="outline" onClick={handlePreview}>
+									<FaEye />
+								</ActionIcon>
+							</Tooltip>
+						</Tooltip.Group>
+					</div>
+				</div>
+			</caption>
+			<thead>
+				<tr>
+					<th>
+						<div className="flex flex-row gap-x-2">
+							<Checkbox
+								className="mr-4"
+								checked={allChecked}
+								indeterminate={indeterminate}
+								transitionDuration={0}
+								onChange={handleAllCheckedChange}
+							/>
+							Major
+						</div>
+					</th>
+					<th>Year</th>
+					<th>Language</th>
+				</tr>
+			</thead>
+			<tbody>
+				<StudyPlanTableRowsTemplate allValues={values} filteredValues={filteredValues} handlers={handlers} />
+			</tbody>
+		</Table>
+	);
+}
---
+++ src/types/study_plans_page_types/StudyPlanElement.ts
@@ -0,0 +1,10 @@
+// This is in its own file because it's used in multiple places
+export interface StudyPlanElement {
+	faculty: string;
+	major: string;
+	year: string;
+	language: string;
+	checked: boolean;
+	fileKey: string;
+	key: string;
+}
