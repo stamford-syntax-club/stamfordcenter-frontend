dfe3a1dc0564afb11cc349450fef895820cd0914
Lxkas <alohasunshineday@yahoo.com>
2023-08-10 21:36:12 +0700

    add original links

--- src/components/page_sections/QuickLinksSection.tsx
+++ src/components/page_sections/QuickLinksSection.tsx
@@ -7,6 +7,7 @@ interface QuickLinkCardProps {
 	title: string;
 	description: string;
 	link: string;
+	originalLink: string;
 	imgUrl: string;
 }
 
@@ -17,61 +18,71 @@ const quickLinkCards = [
 		description:
 			"A platform to find your study material from your instructors, including submitting your assignment.",
 		link: "https://learn.stamford.edu/",
+		originalLink: "https://learn.stamford.edu/",
 	},
 	{
 		imgUrl: "/assets/images/logos/registrar.png",
 		title: "Registrar",
 		description: "A platform to perform ADD/DROP, payment, check GPA, and many more.",
 		link: "https://reg.stamford.edu/registrar/login.asp",
+		originalLink: "https://reg.stamford.edu/registrar/login.asp",
 	},
 	{
 		imgUrl: "/assets/images/logos/microsoft.png",
 		title: "Microsoft365",
 		description: "A main platform provide by STIU for you to use the Microsoft cloud-base products for free.",
 		link: "https://www.microsoft365.com/",
+		originalLink: "https://www.microsoft365.com/",
 	},
 	{
 		imgUrl: "/assets/images/logos/van.png",
 		title: "Van Schedule",
 		description: "A transportation provide by STIU for the students and staff each term.",
 		link: "eee",
+		originalLink: "eee",
 	},
 	{
 		imgUrl: "/assets/images/logos/calender.png",
-		title: "Acadamic Calendar",
+		title: "Academic Calendar",
 		description:
 			"The calendar that tell you about important academic day in each term, including holidays, exam days, and many more.",
 		link: "eee",
+		originalLink: "eee",
 	},
 	{
 		imgUrl: "/assets/images/logos/student-council.png",
 		title: "Student Council",
 		description: "Follow up the events and activities organized and cooperate by the Student Council team.",
 		link: "https://www.instagram.com/studentcouncilstiu/",
+		originalLink: "https://www.instagram.com/studentcouncilstiu/",
 	},
 	{
 		imgUrl: "/assets/images/logos/registrar.png",
 		title: "Student Instagram",
 		description: "An official Instagram of Stamford that regularly update the important activities and events.",
 		link: "https://www.instagram.com/stamfordthailand/",
+		originalLink: "https://www.instagram.com/stamfordthailand/",
 	},
 	{
-		imgUrl: "/assets/images/logos/student-engagment.png",
+		imgUrl: "/assets/images/logos/student-engagement.png",
 		title: "Student Engagement",
 		description:
 			"A department that mainly responsible for the student activities and development, including Scholarship.",
 		link: "https://www.instagram.com/studentengagementstiu/",
+		originalLink: "https://www.instagram.com/studentengagementstiu/",
 	},
 	{
 		imgUrl: "/assets/images/logos/study-plan.png",
-		title: "Study Plan(Curriculum)",
+		title: "Study Plan (Curriculum)",
 		description:
 			"A PDF file that contain all of the subject that your need to study depending on which curriculum you are in.",
-		link: "eee",
+		link: "study_plans",
+		originalLink:
+			"https://learn.stamford.edu/webapps/portal/execute/tabs/tabAction?action=preview&tabId=_96358_1&roles=Student-UG",
 	},
 ];
 
-function QuickLinkCard({ title, description, link, imgUrl }: QuickLinkCardProps) {
+function QuickLinkCard({ title, description, originalLink, link, imgUrl }: QuickLinkCardProps) {
 	return (
 		<Card withBorder className="flex h-[30rem] w-96 max-w-sm flex-col md:h-[22rem] md:max-w-[16rem]">
 			<Card.Section>
@@ -91,9 +102,11 @@ function QuickLinkCard({ title, description, link, imgUrl }: QuickLinkCardProps)
 					</Link>
 				</div>
 				<Tooltip label="open source">
-					<ActionIcon variant="outline" color="blue" size="lg">
-						<FaArrowUpRightFromSquare />
-					</ActionIcon>
+					<Link target="blank" href={originalLink}>
+						<ActionIcon variant="outline" color="blue" size="lg">
+							<FaArrowUpRightFromSquare />
+						</ActionIcon>
+					</Link>
 				</Tooltip>
 			</Card.Section>
 		</Card>
