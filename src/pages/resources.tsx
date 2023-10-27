e95e65842f7735436be0f95e3ff48f891373aaa5
Lxkas <alohasunshineday@yahoo.com>
2023-10-27 18:18:48 +0700

    implement search

--- src/pages/resources.tsx
+++ src/pages/resources.tsx
@@ -1,12 +1,13 @@
 import ResourceCard from "@components/page_components/resources/ResourceCard";
 import ResourceDrawer from "@components/page_components/resources/ResourceDrawer";
 import { TextInput } from "@mantine/core";
-import { useDisclosure } from "@mantine/hooks";
+import { useDebouncedValue, useDisclosure } from "@mantine/hooks";
 import { Resource } from "@ourtypes/resources_page_types/ResourceTypes";
 import { useEffect, useState } from "react";
 
 export default function ResourcesPage() {
 	const [resourceList, setResourceList] = useState<Resource[]>([]);
+	const [searchQuery, setSearchQuery] = useState("");
 
 	useEffect(() => {
 		fetch("https://center-be.stamford.dev/api/resources/resources").then((res) => {
@@ -19,17 +20,24 @@ export default function ResourcesPage() {
 	const [selectedResource, setSelectedResource] = useState(resourceList[0]);
 	const [isDrawerOpen, { open: openDrawer, close: closeDrawer }] = useDisclosure(false);
 
+	// NOTE: This is just in case we have a lot of resources, so it doesn't update the search query on every keystroke.
+	const [debouncedSearchQuery] = useDebouncedValue(searchQuery, 200);
+
+	const filteredResources = resourceList.filter((resource) =>
+		resource.name.toLowerCase().includes(debouncedSearchQuery.toLowerCase()),
+	);
+
 	return (
 		<div className="flex flex-col">
 			{/* Searchbar */}
 			<div className="container mx-auto mb-4 flex w-full flex-row items-center">
 				<div className="w-full">
-					{/* TODO: add the icons back lol */}
+					{/* TODO: Make this search the ResourceFile inside the Resource and maybe bring it up? That might be pretty annoying though */}
 					<TextInput
 						placeholder="Search Resources"
-						// leftSection={<IconSearch style={{ width: rem(12), height: rem(12) }} stroke={1.5} />}
+						value={searchQuery}
+						onChange={(event) => setSearchQuery(event.currentTarget.value)}
 						rightSectionWidth={70}
-						// rightSection={<Code className={classes.searchCode}>Ctrl + K</Code>}
 						styles={{ section: { pointerEvents: "none" } }}
 						className="w-full md:mr-auto md:w-64"
 					/>
@@ -44,7 +52,7 @@ export default function ResourcesPage() {
 					closeDrawer={closeDrawer}
 				/>
 
-				{resourceList.map((resource, index) => (
+				{filteredResources.map((resource, index) => (
 					<ResourceCard
 						key={`resource_${index}`}
 						resource={resource}
