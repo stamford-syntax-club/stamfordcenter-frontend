60e47693b3cc6963d0bb0dccb7471a3fd2425171
chinathaip <cartoonabe@gmail.com>
2023-11-10 22:35:44 +0700

    redesign carousel

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,79 +1,83 @@
-import { Text, Center, Card, Image, Grid, rem } from "@mantine/core";
+import { Text, Center, rem, Accordion, Stack } from "@mantine/core";
+import Image from "next/image";
 import { Carousel } from "@mantine/carousel";
+import AutoPlay from "embla-carousel-autoplay";
 import { ClubAchievement } from "@ourtypes/about_page_types/ClubAchievement";
+import { useRef } from "react";
 
 const achievements: ClubAchievement[] = [
 	{
 		title: "Git & GitHub for Software Engineer Workshop",
 		description:
 			"A practical workshop with more than 40+ attendees that teaches students and lecturers how to use Git and GitHub to manage code and collaborate with others.",
-		images: [
-			{
-				src: "/assets/images/about/github-workshop1.png",
-				alt: "Git & GitHub for Software Engineer Workshop",
-			},
-			{
-				src: "/assets/images/about/github-workshop2.png",
-				alt: "Git & GitHub for Software Engineer Workshop 2",
-			},
-			{
-				src: "/assets/images/logos/study-plan.png",
-				alt: "Club Fair",
-			},
-		],
 	},
 	{
 		title: "Club Fair",
-		description: "Something ",
-		images: [
-			{
-				src: "/assets/images/logos/study-plan.png",
-				alt: "Club Fair",
-			},
-		],
+		description: "Something club fair",
+	},
+];
+
+const carouselImages = [
+	{
+		src: "/assets/images/about/github-workshop1.png",
+		alt: "Git & GitHub for Software Engineer Workshop",
+	},
+	{
+		src: "/assets/images/about/github-workshop2.png",
+		alt: "Git & GitHub for Software Engineer Workshop 2",
+	},
+	{
+		src: "/assets/images/logos/study-plan.png",
+		alt: "Club Fair",
 	},
 ];
 
 export default function AboutUsPage() {
+	const autoplay = useRef(AutoPlay({ delay: 2000 }));
+
+	const slides = carouselImages.map((image, index) => (
+		<Carousel.Slide key={`carouselImages-${index}`}>
+			<Image height={300} width={300} src={image.src} alt={image.alt} />
+		</Carousel.Slide>
+	));
+
 	return (
 		<div className="container mx-auto mt-8 text-center">
-			<div className="mb-6">
-				<Text className="mb-4 text-3xl font-bold uppercase text-white">About us</Text>
-				<Text className="mb-4 text-xl font-bold text-white">Stamford Syntax Club</Text>
-				<Text className="mb-4">
+			<Stack className="mb-6">
+				<Text className="text-3xl font-bold uppercase text-white">About us</Text>
+				<Text className="text-xl font-bold text-white">Stamford Syntax Club</Text>
+				<Text>
 					Stamford Syntax Club is a student-run club at Stamford International University that aims to use
 					technology to improve the lives of students and faculty members. We are a group of students who are
 					passionate about technology and want to make a difference in the Stamford community.
 				</Text>
-				<Text className="mb-4 text-lg font-semibold">Our Past Activities</Text>
+				<Text className="text-lg font-semibold">Our Past Activities</Text>
 
-				{achievements.map((achievement, index) => (
-					<Card key={`achievement-${index}`} className="mx-10 mb-8 p-6">
-						<Grid align="center">
-							<Grid.Col span={{ base: 12, md: 4, lg: 4, xl: 4 }}>
-								<Carousel mx="auto" loop>
-									{achievement.images.map((image, imageIndex) => (
-										<Carousel.Slide key={`achievement-${index}-image-${imageIndex}`}>
-											<Center>
-												<Image
-													radius="md"
-													style={{ width: rem(300), height: rem(300) }}
-													src={image.src}
-													alt={image.alt}
-												/>
-											</Center>
-										</Carousel.Slide>
-									))}
-								</Carousel>
-							</Grid.Col>
-							<Grid.Col span={{ base: 12, md: 8, lg: 8, xl: 8 }}>
-								<Text className="mb-2 text-xl font-bold text-white">{achievement.title}</Text>
+				<Accordion>
+					{achievements.map((achievement, index) => (
+						<Accordion.Item key={`achievement-${index}`} value={`achievemment-${index}`}>
+							<Accordion.Control>
+								<Text className="mb-2 text-lg">{achievement.title}</Text>
+							</Accordion.Control>
+							<Accordion.Panel>
 								<Text className="mb-4">{achievement.description}</Text>
-							</Grid.Col>
-						</Grid>
-					</Card>
-				))}
-			</div>
+							</Accordion.Panel>
+						</Accordion.Item>
+					))}
+				</Accordion>
+
+				<Center>
+					<Carousel
+						mx="auto"
+						loop
+						plugins={[autoplay.current]}
+						onMouseEnter={autoplay.current.stop}
+						onMouseLeave={autoplay.current.reset}
+					>
+						{slides}
+					</Carousel>
+				</Center>
+			</Stack>
 
 			<div className="mb-6">
 				<Text className="mb-4 text-lg font-bold text-white">About Stamford Center</Text>
--- src/types/about_page_types/ClubAchievement.ts
+++ src/types/about_page_types/ClubAchievement.ts
@@ -1,10 +1,4 @@
 export interface ClubAchievement {
 	title: string;
 	description: string;
-	images: ClubAchievementImage[];
-}
-
-export interface ClubAchievementImage {
-	src: string;
-	alt: string;
 }
