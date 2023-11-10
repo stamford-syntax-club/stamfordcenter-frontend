5ee3e94ddfa7a640e32c5709e97dbeb8809c08ce
chinathaip <cartoonabe@gmail.com>
2023-11-11 01:01:14 +0700

    make about stamfordcenter comes first

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,4 +1,5 @@
-import { Text, Center, rem, Accordion, Stack } from "@mantine/core";
+import Link from "next/link";
+import { Text, Center, Accordion, Stack } from "@mantine/core";
 import Image from "next/image";
 import { Carousel } from "@mantine/carousel";
 import AutoPlay from "embla-carousel-autoplay";
@@ -39,15 +40,36 @@ export default function AboutUsPage() {
 		<div className="container mx-auto mt-8 text-center">
 			<Stack className="mb-6">
 				<Text className="text-3xl font-bold uppercase text-white">About us</Text>
-				<Text className="text-xl font-bold text-white">Stamford Syntax Club</Text>
+				<Text className="text-2xl font-bold text-white">Stamford Center</Text>
+				<Text>
+					Stamford Center was conceived to address the pain point that many new students face when trying to
+					locate essential resources on official university websites. Recognizing the frustration of
+					navigating through various platforms for registration, learning tools, and university department
+					communication channels, we set out to create a centralized solution. Stamford Center aims to
+					alleviate this pain point by providing students with a user-friendly hub, simplifying the process of
+					finding critical information and ensuring a smoother transition into university life.
+				</Text>
+
+				<Text className="text-lg font-bold"> Resources on our website</Text>
+				<Text>
+					Resources on our website are taken from official university websites and are updated regularly to be
+					as accurate as possible. We also provide a search function to help you find the information you
+					need. If the resource you are looking for is not available on our website, please inform us by
+					submitting{" "}
+					<Link href="https://forms.office.com/r/z48ExG8dPs" target="_blank">
+						<span className="text-yellow-200">a feedback form here</span>
+					</Link>
+				</Text>
+
+				<Text className="text-2xl font-bold text-white">Stamford Syntax Club</Text>
 				<Text>
 					Stamford Syntax Club is a student-run club at Stamford International University that aims to use
 					technology to improve the lives of students and faculty members. We are a group of students who are
 					passionate about technology and want to make a difference in the Stamford community.
 				</Text>
-				<Text className="text-lg font-semibold">Our Past Activities</Text>
+				<Text className="text-lg font-bold">Our Past Activities</Text>
 
-				<Accordion multiple transitionDuration={1000}>
+				<Accordion multiple>
 					{achievements.map((achievement, index) => (
 						<Accordion.Item key={`achievement-${index}`} value={`achievemment-${index}`}>
 							<Accordion.Control>
@@ -75,18 +97,6 @@ export default function AboutUsPage() {
 					</Carousel>
 				</Center>
 			</Stack>
-
-			<div className="mb-6">
-				<Text className="mb-4 text-lg font-bold text-white">About Stamford Center</Text>
-				<Text>
-					Stamford Center was conceived to address the pain point that many new students face when trying to
-					locate essential resources on official university websites. Recognizing the frustration of
-					navigating through various platforms for registration, learning tools, and university department
-					communication channels, we set out to create a centralized solution. Stamford Center aims to
-					alleviate this pain point by providing students with a user-friendly hub, simplifying the process of
-					finding critical information and ensuring a smoother transition into university life.
-				</Text>
-			</div>
 		</div>
 	);
 }
