3f5ee3b3b579556d04876aa14a6d23cab037fdee
NayHtetKyaw <nayhtetkyaw.dev@gmail.com>
2023-11-16 22:00:34 +0700

    formatted

--- src/pages/contact_information.tsx
+++ src/pages/contact_information.tsx
@@ -1,18 +1,11 @@
-import {
-  Card,
-  Image,
-  Text,
-  Button,
-  Group,
-  CardSection,
-} from "@mantine/core";
+import { Card, Image, Text, Button, Group, CardSection } from "@mantine/core";
 
 interface ContactCardProps {
-  title: string;
-  operationTitle: string;
-  operationTime: string;
-  imgUrl: string;
-  email: string;
+	title: string;
+	operationTitle: string;
+	operationTime: string;
+	imgUrl: string;
+	email: string;
 }
 
 const imageUrl = "/assets/images/logos/registrar.png";
@@ -62,60 +55,53 @@ const contactCards = [
 	},
 ];
 
-const ContactCard = ({
-  title,
-  operationTime,
-  operationTitle,
-  imgUrl,
-  email,
-}: ContactCardProps) => (
-  <Card className="p-10 md:p-10 lg:p-10 max-w-[17rem] mx-auto rounded-lg">
-    <CardSection className="max-w-xs">
-      <Image className="rounded-lg w-full h-full" src={imgUrl} alt="Contact Img" />
-    </CardSection>
+const ContactCard = ({ title, operationTime, operationTitle, imgUrl, email }: ContactCardProps) => (
+	<Card className="mx-auto max-w-[17rem] rounded-lg p-10 md:p-10 lg:p-10">
+		<CardSection className="max-w-xs">
+			<Image className="h-full w-full rounded-lg" src={imgUrl} alt="Contact Img" />
+		</CardSection>
 
-    <CardSection className="mt-4 md:mt-6 lg:mt-8 flex text-white text-lg font-semibold font-open-sans">
-      {title}
-    </CardSection>
+		<CardSection className="font-open-sans mt-4 flex text-lg font-semibold text-white md:mt-6 lg:mt-8">
+			{title}
+		</CardSection>
 
-    <CardSection className="mt-2 md:mt-4 lg:mt-6 flex items-center text-sm md:text-base">
-      {operationTitle}
-      <br />
-      {operationTime}
-    </CardSection>
+		<CardSection className="mt-2 flex items-center text-sm md:mt-4 md:text-base lg:mt-6">
+			{operationTitle}
+			<br />
+			{operationTime}
+		</CardSection>
 
-    <Card.Section className="mt-4">
-      <div className="w-full">
-        <a href={`mailto:${email}`}>
-          <Button className="w-full h-10 text-blue-300 bg-slate-800">Email</Button>
-        </a>
-      </div>
-    </Card.Section>
-  </Card>
+		<Card.Section className="mt-4">
+			<div className="w-full">
+				<a href={`mailto:${email}`}>
+					<Button className="h-10 w-full bg-slate-800 text-blue-300">Email</Button>
+				</a>
+			</div>
+		</Card.Section>
+	</Card>
 );
 
-
 const ContactInfoPage = () => (
-  <div className="container mx-auto mt-12 justify-center">
-    {/* Header */}
-    <h1>
-      <div className="flex flex-col items-center justify-center text-center">
-        <div className="mb-4 text-5xl font-bold text-white">Contact Information</div>
-      </div>
-    </h1>
+	<div className="container mx-auto mt-12 justify-center">
+		{/* Header */}
+		<h1>
+			<div className="flex flex-col items-center justify-center text-center">
+				<div className="mb-4 text-5xl font-bold text-white">Contact Information</div>
+			</div>
+		</h1>
 
-    {/* Header.Description */}
-    <Text className="text-center">
-      Explore the diverse departments below to find the contact information you need. Our dedicated staff is
-      ready to help you on your educational journey. Feel free to reach out, and we'll ensure you get the
-      support and information required to succeed during your time at Stamford
-    </Text>
+		{/* Header.Description */}
+		<Text className="text-center">
+			Explore the diverse departments below to find the contact information you need. Our dedicated staff is ready
+			to help you on your educational journey. Feel free to reach out, and we'll ensure you get the support and
+			information required to succeed during your time at Stamford
+		</Text>
 
-    <Group className="m-8 grid items-center justify-center gap-8 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
-      {contactCards.map((card) => (
-        <ContactCard key={`contactcard-${card.title}`} {...card} />
-      ))}
-    </Group>
-  </div>
+		<Group className="m-8 grid items-center justify-center gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+			{contactCards.map((card) => (
+				<ContactCard key={`contactcard-${card.title}`} {...card} />
+			))}
+		</Group>
+	</div>
 );
 export default ContactInfoPage;
