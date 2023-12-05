aed5914bebc494f464eb0927925ba5d5022bf7a7
Lxkas <alohasunshineday@yahoo.com>
2023-12-05 20:42:19 +0700

    update deps and revert to old dark theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,29 @@
+import "@mantine/carousel/styles.css";
 import "@mantine/core/styles.css";
-import '@mantine/carousel/styles.css';
 import "../styles/globals.css";
 
 import ApplicationShell from "@components/core/ApplicationShell";
-import { MantineProvider } from "@mantine/core";
+import { MantineProvider, createTheme } from "@mantine/core";
 import { Notifications } from "@mantine/notifications";
 import { AppProps } from "next/app";
 import Head from "next/head";
 
-// import { Inter } from "next/font/google";
-// const inter = Inter({ subsets: ["latin"] });
+const theme = createTheme({
+	colors: {
+		dark: [
+			"#C1C2C5",
+			"#A6A7AB",
+			"#909296",
+			"#5c5f66",
+			"#373A40",
+			"#2C2E33",
+			"#25262b",
+			"#1A1B1E",
+			"#141517",
+			"#101113",
+		],
+	},
+});
 
 export default function App(props: AppProps) {
 	const { Component, pageProps } = props;
@@ -21,7 +35,7 @@ export default function App(props: AppProps) {
 				<meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
 			</Head>
 
-			<MantineProvider defaultColorScheme="dark">
+			<MantineProvider theme={theme} defaultColorScheme="dark">
 				<Notifications />
 				<ApplicationShell>
 					<Component {...pageProps} />
